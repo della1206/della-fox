90f5c14e643ec3590e66cc2e91b0cb5acd2f53a8
della <della24si@mahasiswa.pcr.ac.id>
2025-09-23 10:11:07 +0700

    Latihan Passing Data dan Git

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -6,5 +6,35 @@ use Illuminate\Http\Request;
 
 class PegawaiController extends Controller
 {
-    //
+    public function index()
+    {
+        $name = "Della Marcelina";
+        $birthDate = Carbon::create(2006, 9, 17);
+        $tglHarusWisuda = Carbon::create(2028, 10, 30);
+
+        $my_age = $birthDate->age; // hitung umur otomatis
+        $hobbies = ["Membaca", "Menulis", "Coding", "Olahraga", "Mendengarkan Musik"];
+        $time_to_study_left = now()->diffInDays($tglHarusWisuda);
+        $current_semester = 3;
+        $future_goal = "Menjadi Software Engineer";
+
+        // Cek semester
+        if ($current_semester < 3) {
+            $semester_info = "Masih Awal, Kejar TAK";
+        } else {
+            $semester_info = "Jangan main-main, kurangi malas!";
+        }
+
+        // Passing data ke view
+        return view('pegawai.index', [
+            'name' => $name,
+            'my_age' => $my_age,
+            'hobbies' => $hobbies,
+            'tgl_harus_wisuda' => $tglHarusWisuda->toDateString(),
+            'time_to_study_left' => $time_to_study_left,
+            'current_semester' => $current_semester,
+            'semester_info' => $semester_info,
+            'future_goal' => $future_goal
+        ]);
+    }
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data Pegawai</title>
+</head>
+<body>
+    <h1>Profil Saya</h1>
+    <p><b>Nama:</b> {{ $name }}</p>
+    <p><b>Umur:</b> {{ $my_age }} tahun</p>
+    <p><b>Hobi:</b></p>
+    <ul>
+        @foreach($hobbies as $hobi)
+            <li>{{ $hobi }}</li>
+        @endforeach
+    </ul>
+    <p><b>Tanggal Harus Wisuda:</b> {{ $tgl_harus_wisuda }}</p>
+    <p><b>Sisa Hari Menuju Wisuda:</b> {{ $time_to_study_left }} hari</p>
+    <p><b>Semester Saat Ini:</b> {{ $current_semester }}</p>
+    <p><b>Info Semester:</b> {{ $semester_info }}</p>
+    <p><b>Cita-cita:</b> {{ $future_goal }}</p>
+</body>
+</html>

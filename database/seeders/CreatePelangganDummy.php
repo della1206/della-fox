858eda2df3e499bfc3f486d701a9c2fc7bb5f84a
vanesya24si-tech <vanesya24si@mahasiswa.pcr.ac.id>
2025-11-11 07:50:48 +0700

    Penambahan seeder dan user

---
+++ database/seeders/CreatePelangganDummy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+use Faker\Factory;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CreatePelangganDummy extends Seeder
+{
+
+    public function run(): void
+    {
+    $faker = Factory::create();
+
+    foreach (range(1, 100) as $index) {
+        DB::table('pelanggan')->insert([
+            'first_name' => $faker->firstName,
+            'last_name'  => $faker->lastName,
+            'birthday'   => $faker->date('Y-m-d', '2005-12-31'),
+            'gender'     => $faker->randomElement(['Male', 'Female', 'Other']),
+            'email'      => $faker->unique()->safeEmail,
+            'phone'      => $faker->phoneNumber,
+        ]);
+    }
+}
+    }

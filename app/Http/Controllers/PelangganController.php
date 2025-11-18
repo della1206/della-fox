dd6d7882341eb4b6f31c639ed8430f785066ae8e
vanesya24si-tech <vanesya24si@mahasiswa.pcr.ac.id>
2025-11-18 07:26:55 +0700

    penerapan pagination

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -11,7 +11,7 @@ class PelangganController extends Controller
      */
     public function index()
     {
-        $data['dataPelanggan'] = Pelanggan::all();
+        $data['dataPelanggan'] = Pelanggan::simplePaginate(10);
         return view('admin.pelanggan.index', $data);
     }
 
